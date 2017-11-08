a168c16c736cf7846649c31db065df9ae4e9ff9f
SeanCarroll17 <31080468+SeanCarroll17@users.noreply.github.com>
2017-11-08 02:44:01 +0000

    Update problem1.js

--- problem1.js
+++ problem1.js
@@ -1,10 +1,10 @@
-function Prog1()
-{
-	var sum = 0;
+function Prog1(){
 	var a=document.getElementById("inp1").value;
-	for(var i = 1; i < a ; i++)
-		if(i%3==0||i%5==0)
-			sum += i;
+	for(var i = 1; i < a ; i++){
+		if(i%3==0||i%5==0){
+			var sum= i;
+		}
+	}
 	document.getElementById('out1').value=sum;
 }
 	
